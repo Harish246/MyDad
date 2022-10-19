a8892e2bbfbe1ec070bdfa12dcebd1aa02fe6ddb
Harish246 <hari128m035@gmail.com>
2022-10-19 11:39:52 +0530

    nuvu1

---
+++ MyDad/src/main/java/Ionly/Meall.java
@@ -0,0 +1,14 @@
+package Ionly;
+
+public class Meall {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r=3;
+		int m=10;
+		int k=r+m;
+		System.out.println("one of one"+k);
+
+	}
+
+}
